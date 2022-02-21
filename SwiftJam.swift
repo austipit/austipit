9afc3777cad11767ef6aae3ad64da78b751bc43e
Aus <100020200+austipit@users.noreply.github.com>
2022-02-21 09:55:31 -0500

    Create SwiftJam.swift

---
+++ SwiftJam.swift
@@ -0,0 +1,16 @@
+/*
+
+If-else statements, about two teams regarding who won based on their number of points
+
+*/
+
+let tuneSquadPoints = 78
+let monstarsPoints = 77
+
+// Write your code below 🏀
+if tuneSquadPoints<monstarsPoints{
+  print("Tune Squad has been eliminated!")
+} else {
+  print("Monstars, you have won!")
+}
+
