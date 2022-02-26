5c4aa6421afc74c8ea087e180dc8c1ba56f3f61f
Aus <100020200+austipit@users.noreply.github.com>
2022-02-26 15:47:50 -0500

    Create magic8.py

---
+++ magic8.py
@@ -0,0 +1,32 @@
+# 8 ball that provides 1 of 9 random answers to the specified user's name and question
+
+import random
+random_number = random.randint(1,9)
+#print(random_number)
+name = "Austin"
+question = "Will I land an SDE role?"
+answer = ""
+
+if random_number == 1:
+  answer = "Yes - definitely."
+elif random_number == 2:
+  answer = "It is decidely so."
+elif random_number == 3:
+  answer = "Without a doubt."
+elif random_number == 4:
+  answer = "Reply hazy, try again"
+elif random_number == 5:
+  answer = "Ask again later."
+elif random_number == 6:
+  answer = "Better not tell you now."
+elif random_number == 7:
+  answer = "My sources say no."
+elif random_number == 8:
+  answer = "Outlook not so good."
+elif random_number == 9:
+  answer = "Very doubtful."
+else:
+  answer = "Error"
+
+print(name + " asks: " + question)
+print("Magic 8-Ball's answer: " + answer)
