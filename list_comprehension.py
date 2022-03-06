b81edba6f5a7f99f148647915f19de11666754c3
Aus <100020200+austipit@users.noreply.github.com>
2022-03-06 03:25:02 -0500

    Create list_comprehension.py

---
+++ list_comprehension.py
@@ -0,0 +1,10 @@
+# Your code below:
+single_digits = range(10)
+squares = []
+cubes = []
+for digit in single_digits:
+  print(digit)
+  squares.append(digit**2)
+  print(squares)
+  cubes.append(digit**3)
+  print(cubes)
