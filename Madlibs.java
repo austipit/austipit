aaa4f58b9689ba2edeb81ea604be5f6dc72141b0
Aus <100020200+austipit@users.noreply.github.com>
2022-07-13 14:45:14 -0400

    Create Madlibs.java
    Change the data in each variable and create your own crazy story!

---
+++ Madlibs.java
@@ -0,0 +1,35 @@
+public class MadLibs {
+  /*
+  The purpose of this program is to generate a mad libbed story.
+  Author: Austin
+  Today's Date: 07/13/2022'
+  */
+  	public static void main(String[] args){
+      
+      //The template for the story
+       String name1 = "Austin";
+      String adjective1 = "rusty";
+      String adjective2 = "colorful";
+      String adjective3 = "dry";
+      String verb1 = "vault";
+      String noun1 = "London";
+      String noun2 = "Mustang";
+      String noun3 = "Franklin";
+      String noun4 = "Xbox";
+      String noun5 = "Kali Linux";
+      String noun6 = "Java";
+      String name2 = "Kaitlyn";
+      int number = 17;
+      String place1 = "Fort Meade";
+     
+      String story = "This morning "+name1+" woke up feeling "+adjective1+". 
+        'It is going to be a "+adjective2+" day!' Outside, a bunch of "+noun1+"s were protesting to keep "+noun2+" in stores. 
+        They began to "+verb1+" to the rhythm of the "+noun3+", which made all the "+noun4+"s very "+adjective3+". 
+        Concerned, "+name1+" texted "+name2+", who flew "+name1+" to "+place1+" and dropped "+name1+" in a puddle of frozen "+noun5+". 
+        "+name1+" woke up in the year "+number+", in a world where "+noun6+"s ruled the world.";
+      
+ System.out.print(story);
+
+
+    }       
+}
